3e81f9558956b8f575016d522cacc2d9e6297dfe
xtream2000c <carlmarcast@gmail.com>
2022-02-01 19:53:11 +0100

    Añadida vista noticias, y enlaces a las vistas cambiados, ya funciona el navegador

---
+++ JS/scriptNoticias.js
@@ -0,0 +1,18 @@
+window.onload = main;
+
+function main(){
+    var noticias = JSON.parse(sessionStorage.getItem("noticias"));
+
+    for(let i=noticias.length-1;i>=0;i--){
+        let noticia = noticias[i];
+        
+        $('#noticias .row').append(`<div class="card col-12 mt-3 bg-info">
+            <div class "card-body">
+                <h1 class="card-title">${noticia.titulo_noticia}</h1>
+                <p class="card-text">${noticia.cuerpo_noticia}</p><br>
+            </div>
+        </div>`);
+        
+            
+    }
+}
