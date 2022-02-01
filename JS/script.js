19afcf0c4fc39c438feabe466ee6c09f2e6f7eb9
RonGarciaGuerrero <ronald.garcia.arq@gmail.com>
2022-02-01 13:28:58 +0100

    añado eventos.php, sustituyo html a pelo en noticias y eventos por el resultado de  la consulta del script.js en la bbdd

--- JS/script.js
+++ JS/script.js
@@ -8,16 +8,14 @@ function main() {
         dataType: "JSON",
         success : function(infoInstalaciones){
             console.log(infoInstalaciones);
-            if(1==1 || infoInstalaciones.status === "success"){//esto es para que entre en el if siempre, es probable que no haga falta el if
+            if(1==1 || infoInstalaciones.status === "success"){//esto es para que entre en el if siempre, es probable que no haga falta el if, el objeto que se recibe como parametro es el json, ese json no tiene success y nunca va a dar true
                 alert("success");
                 sessionStorage.setItem("instalaciones", JSON.stringify(infoInstalaciones));
                 
-                for(let i=0;i<infoInstalaciones.length;i++){//se itera sobre el array
-
+                for(let i=0;i<infoInstalaciones.length;i++){//se itera sobre el array y se imprimen las instalaciones
                     let inst = infoInstalaciones[i];
-                    $('#tablaInstalaciones').append(`<tr><td><h6>${inst.tipo_pista}</h6></td><td>${inst.precio}</td><td>${inst.precio_no_socio}</td></tr>`);
+                    $('#tablaInstalaciones').append(`<tr><td><h6>${inst.tipo_pista}</h6></td><td><h6>${inst.precio}</h6></td><td><h6>${inst.precio_no_socio}</h6></td></tr>`);
                 }
-
             }else{
                 alert("error");
             }
@@ -28,13 +26,26 @@ function main() {
     })
     $.ajax({
         type:"POST",
-        url: "noticias.php",
+        url: "PHP/noticias.php",
         dataType: "JSON",
         success : function(noticias){
             console.log(noticias);
-            if(noticias.status === "success"){
+            if(1==1 || noticias.status === "success"){//mismo caso que instalaciones, conviene quitar el if
                 alert("success");
                 sessionStorage.setItem("noticias", JSON.stringify(noticias));
+                console.log('probando if noticias');
+                for(let i=0;i<noticias.length;i++){
+                    let notic = noticias[i];
+                    if(i==0){//solo en la primera iteración se pone data-bs-slide-to="0" y en el inner la clase active, luego el el else voy añadiendo mas data-bs-slide-to [i], y en el inner ya no lleva la clase active
+                        console.log('probando noticias dentro del for');
+                        $('#noticias .carousel-indicators').append(`<button type="button" data-bs-target="#noticias" data-bs-slide-to="0" class="active"></button>`);
+                        $('#noticias .carousel-inner').append(`<div class="carousel-item active"><h1>${notic.titulo_noticia}</h1><p>${notic.cuerpo_noticia}</p><br></div>`);    
+                    }else{
+                        $('#noticias .carousel-indicators').append(`<button type="button" data-bs-target="#noticias" data-bs-slide-to="${i}" class="active"></button>`);
+                        $('#noticias .carousel-inner').append(`<div class="carousel-item"><h1>${notic.titulo_noticia}</h1><p>${notic.cuerpo_noticia}</p><br></div>`);
+                    }   
+                }
+
             }else{
                 alert("error");
             }
@@ -45,15 +56,22 @@ function main() {
     })
     $.ajax({
         type:"POST",
-        url: "eventos.php",
+        url: "PHP/eventos.php",
         dataType: "JSON",
         success : function(eventos){
             console.log(eventos);
-            if(eventos.status === "success"){
-                alert("success");
-                sessionStorage.setItem("eventos", JSON.stringify(eventos));
-            }else{
-                alert("error");
+            alert("success");
+            sessionStorage.setItem("eventos", JSON.stringify(eventos));
+            for(let i=0;i<eventos.length;i++){
+                let even = eventos[i];
+                console.log('probando eventos');
+                if(i==0){//solo en la primera iteración se pone data-bs-slide-to="0" y en el inner la clase active, luego el el else voy añadiendo mas data-bs-slide-to [i], y en el inner ya no lleva la clase active
+                    $('#eventos .carousel-indicators').append(`<button type="button" data-bs-target="#eventos" data-bs-slide-to="0" class="active"></button>`);
+                    $('#eventos .carousel-inner').append(`<div class="carousel-item active"><h1>${even.titulo_evento}</h1><p>${even.cuerpo_evento}</p><br></div>`);
+                }else{
+                    $('#eventos .carousel-indicators').append(`<button type="button" data-bs-target="#eventos" data-bs-slide-to="${i}" class="active"></button>`);
+                    $('#eventos .carousel-inner').append(`<div class="carousel-item"><h1>${even.titulo_evento}</h1><p>${even.cuerpo_evento}</p><br></div>`);
+                }
             }
         },
         error : function(XHR, status){
---
+++ PHP/eventos.php
@@ -0,0 +1,31 @@
+<?php
+
+//Requerimiento de acceso a base de datos.
+require_once(dirname(__FILE__).'/../PHP/DB.php');
+
+class Eventos{ 
+
+    public $titulo_evento;
+    public $cuerpo_evento;
+    public $id_eventos;
+
+    function __construct($titulo_evento,$cuerpo_evento,$id_evento){
+        $this->titulo_evento=$titulo_evento;
+        $this->cuerpo_evento=$cuerpo_evento;
+        $this->id_evento=$id_evento;
+    }
+
+    static function getEventos(){
+        $sentencia = "SELECT * FROM eventos";
+        $result = mysqli_fetch_all(DB::query($sentencia),MYSQLI_ASSOC);
+        $eventos = Array();
+        foreach($result as $evento){
+            array_push($eventos, new Eventos($evento["titulo_evento"],$evento["cuerpo_evento"],$evento["id_eventos"]));
+        }
+
+        return json_encode($eventos);
+    }
+
+}
+print(Eventos::getEventos());//imprime todas los eventos en JSON, llama a getEventos()
+?>
