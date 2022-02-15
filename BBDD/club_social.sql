d4b9eefcc11891f5d431e6ee41655172d68d9971
RonGarciaGuerrero <ronald.garcia.arq@gmail.com>
2022-02-15 15:56:29 +0100

    actualizo el sql con las restricciones de la bbdd

--- BBDD/club_social.sql
+++ BBDD/club_social.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-02-2022 a las 19:38:32
--- Versión del servidor: 10.4.20-MariaDB
--- Versión de PHP: 7.4.22
+-- Tiempo de generación: 15-02-2022 a las 15:55:20
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,79 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `eventos`
---
-
-CREATE TABLE `eventos` (
-  `id_eventos` int(11) NOT NULL,
-  `titulo_evento` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `cuerpo_evento` longtext COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `eventos`
---
-
-INSERT INTO `eventos` (`id_eventos`, `titulo_evento`, `cuerpo_evento`) VALUES
-(1, 'título evento 1', 'cuerpo evento 1'),
-(2, 'título evento 2', 'cuerpo evento 2'),
-(3, 'título evento 3', 'cuerpo evento 4'),
-(4, 'título evento 4', 'cuerpo evento 4'),
-(5, 'título evento 5', 'cuerpo evento 5');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `instalaciones`
---
-
-CREATE TABLE `instalaciones` (
-  `nombre_pista` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `tipo_pista` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `precio` int(11) NOT NULL,
-  `precio_no_socio` float NOT NULL,
-  `id_pista` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `instalaciones`
---
-
-INSERT INTO `instalaciones` (`nombre_pista`, `tipo_pista`, `precio`, `precio_no_socio`, `id_pista`) VALUES
-('padel_1', 'padel', 1, 1.5, 1),
-('padel_2', 'padel', 1, 1.4, 2),
-('tenis_1', 'tenis', 1, 1.6, 3),
-('tenis_2', 'tenis', 1, 1.4, 4),
-('futbol_1', 'futbol', 5, 1, 5),
-('baloncesto_1', 'baloncesto', 3, 1, 6),
-('barbacoa_1', 'barbacoa', 5, 1, 7);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `noticias`
---
-
-CREATE TABLE `noticias` (
-  `id_noticias` int(11) NOT NULL,
-  `titulo_noticia` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `cuerpo_noticia` longtext COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `noticias`
---
-
-INSERT INTO `noticias` (`id_noticias`, `titulo_noticia`, `cuerpo_noticia`) VALUES
-(1, 'Título noticia 1', 'Cuerpo noticia 1'),
-(2, 'Título noticia 2', 'Cuerpo noticia 2'),
-(3, 'Título noticia 3', 'Cuerpo noticia 3'),
-(4, 'Título noticia 4', 'Cuerpo noticia 4'),
-(5, 'Título noticia 5', 'Cuerpo noticia 5');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `reservas`
 --
 
@@ -105,102 +32,68 @@ CREATE TABLE `reservas` (
   `id_usuario` int(11) NOT NULL,
   `id_pista` int(11) NOT NULL,
   `fecha` date NOT NULL,
-  `hora` varchar(100) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `apellidos` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `tipo_usuario` set('Socio','Administrador','Presidente','Invitado') COLLATE utf8_spanish_ci NOT NULL DEFAULT 'Invitado',
-  `edad` int(11) NOT NULL,
-  `miembros` int(11) NOT NULL,
-  `email` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `usuario` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `contraseña` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+  `hora` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Volcado de datos para la tabla `reservas`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `tipo_usuario`, `edad`, `miembros`, `email`, `usuario`, `contraseña`) VALUES
-(1, 'nombre 1', 'apes1', 'Invitado', 22, 2, 'email@gsfgda', 'usua1', '1234'),
-(2, 'nombre 2', 'apes2', 'Invitado', 2, 2, 'email@gsfgdaegfrg', 'usua2', '25252');
+INSERT INTO `reservas` (`id_reserva`, `id_usuario`, `id_pista`, `fecha`, `hora`) VALUES
+(1, 1, 6, '2022-02-12', 9),
+(2, 5, 1, '2022-02-16', 9),
+(3, 3, 2, '2022-02-13', 9),
+(4, 3, 2, '2022-02-13', 10),
+(5, 3, 2, '2022-02-13', 11),
+(6, 3, 2, '2022-02-13', 12),
+(7, 3, 2, '2022-02-13', 13),
+(8, 3, 2, '2022-02-13', 14),
+(9, 3, 2, '2022-02-13', 15),
+(10, 3, 2, '2022-02-13', 16),
+(11, 3, 2, '2022-02-13', 17),
+(12, 3, 2, '2022-02-13', 18),
+(13, 3, 2, '2022-02-13', 19),
+(14, 3, 2, '2022-02-13', 20),
+(15, 3, 2, '2022-02-13', 21),
+(16, 3, 2, '2022-02-13', 22),
+(28, 1, 4, '2022-02-17', 22),
+(29, 1, 4, '2022-02-15', 12),
+(30, 1, 4, '2022-02-20', 9),
+(31, 1, 1, '2022-02-16', 19);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `eventos`
---
-ALTER TABLE `eventos`
-  ADD PRIMARY KEY (`id_eventos`);
-
---
--- Indices de la tabla `instalaciones`
---
-ALTER TABLE `instalaciones`
-  ADD PRIMARY KEY (`id_pista`);
-
---
--- Indices de la tabla `noticias`
---
-ALTER TABLE `noticias`
-  ADD PRIMARY KEY (`id_noticias`);
-
---
 -- Indices de la tabla `reservas`
 --
 ALTER TABLE `reservas`
-  ADD PRIMARY KEY (`id_reserva`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_reserva`),
+  ADD UNIQUE KEY `RESTRICCION_DUPLICIDAD` (`fecha`,`hora`,`id_pista`) USING BTREE,
+  ADD KEY `FK_reserva_pista` (`id_pista`),
+  ADD KEY `FK_reserva_usuario` (`id_usuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `eventos`
---
-ALTER TABLE `eventos`
-  MODIFY `id_eventos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT de la tabla `instalaciones`
---
-ALTER TABLE `instalaciones`
-  MODIFY `id_pista` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT de la tabla `noticias`
---
-ALTER TABLE `noticias`
-  MODIFY `id_noticias` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
 -- AUTO_INCREMENT de la tabla `reservas`
 --
 ALTER TABLE `reservas`
-  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
--- AUTO_INCREMENT de la tabla `usuarios`
+-- Restricciones para tablas volcadas
 --
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Filtros para la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD CONSTRAINT `FK_reserva_pista` FOREIGN KEY (`id_pista`) REFERENCES `instalaciones` (`id_pista`),
+  ADD CONSTRAINT `FK_reserva_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
