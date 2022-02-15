7bbcc458d193711f1b404e477da910c7a561458b
xtream2000c <carlmarcast@gmail.com>
2022-02-15 18:37:36 +0100

    Footer añadido a todas las vistas, y contacto.html ya funciona, envia correos a la cuenta del club social

---
+++ JS/scriptContacto.js
@@ -0,0 +1,39 @@
+window.onload = main;
+
+function main() {
+    
+    document.getElementById("contacto").addEventListener("submit", enviarCorreo, false)
+
+}
+
+function enviarCorreo(event) {
+    event.preventDefault();
+
+    let nombre = document.getElementById("nombre").value;
+    let correo = document.getElementById("email").value;
+    let asunto = document.getElementById("asunto").value;
+    let cuerpo = document.getElementById("cuerpo").value;
+
+
+
+
+    $.ajax({
+        type:"POST",
+        url: "PHP/contacto.php",//se modifica la ruta
+        data: {
+            'nombre': nombre, 'correo' : correo, 'asunto' : asunto, 'cuerpo' : cuerpo
+        },
+        success : function(enviado){
+
+            alert("El correo se envio de forma exitosa");
+            location.href='index.html';
+
+        },
+
+        error : function(xhr,ajaxOptions, thrownError){
+            alert(xhr.status);
+            alert(thrownError);
+
+        }
+    })
+}
